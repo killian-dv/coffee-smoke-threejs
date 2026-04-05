3b8c91d10d3332c1fc89cf283355c2ae06bd4fd0
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-04-05 21:39:06 +0200

    ✨ : update smoke shader adding wind effect

--- src/shaders/coffee-smoke/vertex.glsl
+++ src/shaders/coffee-smoke/vertex.glsl
@@ -14,9 +14,16 @@ void main() {
   vec3 newPosition = position;
 
   // twist
-  float twistPerlin = texture2D(uPerlinTexture, vec2(0.5, uv.y * 0.2 - uTime * 0.005)).r;
+  float twistPerlin = texture(uPerlinTexture, vec2(0.5, uv.y * 0.2 - uTime * 0.005)).r;
   float angle = twistPerlin * 10.0;
   newPosition.xz = rotate2D(newPosition.xz, angle);
+
+  // wind
+  vec2 windOffset = vec2(
+    texture(uPerlinTexture, vec2(0.25, uTime * 0.01)).r - 0.5,
+    texture(uPerlinTexture, vec2(0.75, uTime * 0.01)).r - 0.5);
+  windOffset *= pow(uv.y, 3.0) * 10.0;
+  newPosition.xz += windOffset;
 
   // final position
   gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
