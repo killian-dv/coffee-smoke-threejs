f89184cfa8967a46080447f439c96c183143687c
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-04-05 19:35:35 +0200

    ✨ : enhance smoke shader with remapping and edge effects

--- src/shaders/coffee-smoke/fragment.glsl
+++ src/shaders/coffee-smoke/fragment.glsl
@@ -12,6 +12,16 @@ void main() {
 
   // smoke 
   float smoke = texture(uPerlinTexture, smokeUv).r;
+
+  // remap
+  smoke = smoothstep(0.4, 1.0, smoke);
+
+  // edges
+  smoke *= smoothstep(0.0, 0.1, vUv.x);
+  smoke *= smoothstep(1.0, 0.9, vUv.x);
+  smoke *= smoothstep(0.0, 0.1, vUv.y);
+  smoke *= smoothstep(1.0, 0.4, vUv.y);
+
   gl_FragColor = vec4(vec3(1.0), smoke);
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
